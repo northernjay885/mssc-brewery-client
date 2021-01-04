b01156fc38f580333944bcdd2bb478d5f88b13df
luorenjie <lrj193927@gmail.com>
2021-01-04 10:44:35 -0500

    add PUT

--- src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
+++ src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.net.URI;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -20,5 +21,18 @@ class BreweryClientTest {
         BeerDto dto = client.getBeerById(UUID.randomUUID());
 
         assertNotNull(dto);
+    }
+
+    @Test
+    void testSaveNewBeer() {
+        //given
+        BeerDto beerDto = BeerDto.builder().beerName("New Beer").build();
+
+        URI uri = client.saveNewBeer(beerDto);
+
+        assertNotNull(uri);
+
+        System.out.println(uri.toString());
+
     }
 }
